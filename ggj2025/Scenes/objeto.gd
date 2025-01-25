88298e5927e921aecca456c8d27818958008413e
Mapachana <anabuenrua99@gmail.com>
2025-01-25 01:40:02 +0100

    Sistema de audio casi bien

--- ggj2025/Scenes/objeto.gd
+++ ggj2025/Scenes/objeto.gd
@@ -9,16 +9,22 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
+	# Si pulso el botón cambia que sonido reproduce. Esto luego se cambia a si tomo una decision u otra
+	if Input.is_action_just_pressed("ui_accept") and not $AudioStreamPlayer.playing:		
+		$AudioStreamPlayer.stream.set_list_stream(1, load("res://Music/prueba/explotar.mp3"))
+	
 	pass
 
 
 func _on_body_entered(body: Node2D) -> void:
 	$Sprite2D.hide()
 	$CollisionShape2D.queue_free()
+	ScriptGlobal.nodo_musica.stop()
 	$AudioStreamPlayer.play()
 	pass # Replace with function body.
 
 
 func _on_audio_stream_player_finished() -> void:
+	ScriptGlobal.nodo_musica.play()
 	queue_free()
 	pass # Replace with function body.
--- ggj2025/Scenes/prueba.gd
+++ ggj2025/Scenes/prueba.gd
@@ -1,10 +1,11 @@
-extends Node
+extends Node2D
 
-var nodo_musica = null
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+
+func _ready():
+	ScriptGlobal.nodo_musica = $AudioStreamPlayer
+	print(ScriptGlobal.nodo_musica)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
