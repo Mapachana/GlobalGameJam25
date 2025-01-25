65cfa4edfa808d81669bd80f3d1bb46744d75469
Lorena Cáceres Arias <lorenax230403@gmail.com>
2025-01-26 00:46:56 +0100

    mas cosasss

--- ggj2025/Scenes/objeto.gd
+++ ggj2025/Scenes/objeto.gd
@@ -15,13 +15,19 @@ var ruta = "res://Music/prueba/"
 
 # Animacion que va a reproducir durante el recuerpo, va de 1 a 6
 @export var anim: int
-
+@onready var sprite = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	perso_en_area = false
 	$Sprite2D.texture = imagen #lo que haya metido de var
+	if $AnimatedSprite2D:
+		sprite = $AnimatedSprite2D
+	else:
+		sprite = $Sprite2D
 	
+	sprite.material.set_shader_parameter("aura_color", Color(1, 1.0, 1.0, 1.0));
+
 	
 	pass # Replace with function body.
 
@@ -78,9 +84,11 @@ func _on_body_entered(body: Node2D) -> void:
 		tween.tween_property(
 			self,                  # Nodo objetivo
 			"scale",             # Propiedad a interpolar
-			Vector2(1.1,1.1),         # Valor final
+			Vector2(1.15,1.15),         # Valor final
 			0.3         # Duración
 		).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
+		sprite.material.set_shader_parameter("aura_width", 4);
+		sprite.material.set_shader_parameter("aura_color", Color(0.0, 1.0, 1.0, 1.0));
 	
 	pass 
 	
@@ -104,6 +112,8 @@ func cargar_recurso(rec) -> AudioStream:
 func desaparecer():	
 	await get_tree().create_timer(0.6).timeout
 	$Sprite2D.hide()
+	if $AnimatedSprite2D :
+		$AnimatedSprite2D.hide()
 	$CollisionShape2D.queue_free()
 	#queue_free()
 
@@ -119,3 +129,6 @@ func _on_body_exited(body: Node2D) -> void:
 		Vector2(1,1),         # Valor final
 		0.3        # Duración
 	).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
+	
+	sprite.material.set_shader_parameter("aura_width", 2.5);
+	sprite.material.set_shader_parameter("aura_color", Color(1, 1, 1, 1));
