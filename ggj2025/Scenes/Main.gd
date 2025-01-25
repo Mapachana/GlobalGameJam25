014e006ecf5b394733e74f201bd87dc6308c45d6
Mapachana <anabuenrua99@gmail.com>
2025-01-25 10:02:51 +0100

    integrado sistema de musica

--- ggj2025/Scenes/Main.gd
+++ ggj2025/Scenes/Main.gd
@@ -10,6 +10,7 @@ var room = 0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	ScriptGlobal.nodo_musica = $AudioStreamPlayer
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
