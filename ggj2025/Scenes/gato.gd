88e72d3a4433273fc7a5d1275e7e6a74149042ca
Mapachana <anabuenrua99@gmail.com>
2025-01-25 20:59:01 +0100

    gato acariciable

--- ggj2025/Scenes/gato.gd
+++ ggj2025/Scenes/gato.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 var animacion_actual = null
 
+var acariciable = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	animacion_actual = "andar"
@@ -10,15 +12,25 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
+	if acariciable and Input.is_action_pressed("ui_accept"):
+		print("ACARICIANDO AL GATO MIAU MIAU")
 	pass
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("pj") and Input.is_action_pressed("ui_accept"):
-		print("Acaricio al gatito miau")
-		# change_anim("acariciar")	
+	if body.is_in_group("pj"):
+		print("acariciable")
+		acariciable = true
+		# change_anim("quieto")	
 	pass # Replace with function body.
 
 func change_anim(animacion):
 	if animacion != animacion_actual:
 		$Sprite2D/AnimationPlayer.play(animacion)
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("pj"):
+		acariciable = false
+	pass # Replace with function body.
