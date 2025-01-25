047a297193352be0b97a4470b1f8e71b8a14f5ce
lorenax55 <51710964+lorenax55@users.noreply.github.com>
2025-01-25 01:02:27 +0100

    el pj se mueve mas

--- ggj2025/Scenes/PJ.gd
+++ ggj2025/Scenes/PJ.gd
@@ -1,14 +1,59 @@
 extends CharacterBody2D
 
 # Constantes para ajustar velocidad y aceleración
-const SPEED = 300
+const SPEED = 350
 const ACCELERATION = 800
-const DRAG = 500
+const DRAG = 300
+
+const MAX_ANGLE = 15 #cosas tween
+const ROTATION_DURATION = 1.0
+
+# Referencia al Tween
+var tween = null
+
+func _ready():
+	tween = get_tree().create_tween()
+
+	rotation = deg_to_rad(-MAX_ANGLE)
+
+	start_rotation()
+
+
+func start_rotation():
+	tween.tween_property(
+		self,                  # Nodo objetivo
+		"rotation",             # Propiedad a interpolar
+		deg_to_rad(MAX_ANGLE),         # Valor final
+		ROTATION_DURATION         # Duración
+	).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+
+	tween.connect("finished",Callable(self,"_on_tween_completed"))
+
+func _on_tween_completed():
+	# Cambiar la dirección de rotación
+	if tween:  # Si hay un tween activo, detenerlo
+		tween.kill()
+		
+	tween = get_tree().create_tween()
+
+	var target_angle = 0
+	if rotation == deg_to_rad(MAX_ANGLE):
+		target_angle = deg_to_rad(-MAX_ANGLE)
+	else: 
+		deg_to_rad(MAX_ANGLE)
+		
+	tween.tween_property(
+		self,                  # Nodo objetivo
+		"rotation",             # Propiedad a interpolar
+		deg_to_rad(MAX_ANGLE),         # Valor final
+		ROTATION_DURATION         # Duración
+	).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 
 func _physics_process(delta):
 	# Obtén las direcciones de entrada del jugador
 	var direction_R = Input.get_axis("ui_left", "ui_right")
 	var direction_U = Input.get_axis("ui_up", "ui_down")
+
 
 	# Movimiento horizontal con aceleración
 	if direction_R != 0:
