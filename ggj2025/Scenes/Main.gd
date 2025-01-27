9d468bd63c8c4bdaede4dcc39dbc503b2c635cc1
Mapachana <anabuenrua99@gmail.com>
2025-01-27 15:43:52 +0100

    Arreglo animacion final

--- ggj2025/Scenes/Main.gd
+++ ggj2025/Scenes/Main.gd
@@ -9,7 +9,7 @@ extends Node2D
 @onready var animacionE5 = $EstrofaAnim4/AnimationPlayer
 @onready var animacionE6 = $EstrofaAnim5/AnimationPlayer
 
-const DURACION_ESTROFA = 44 #CAMBIAR ANTES DEL FINAL
+const DURACION_ESTROFA = 1 #CAMBIAR ANTES DEL FINAL
 const DURACION_MINITRANS = 1
 
 #variables de cosas del control de cámara
@@ -86,9 +86,9 @@ func begin_game():
 	).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 	
 	#CAMBIAR ANTES DEL FINAL
-	tween.connect("finished",Callable(self,"comic_anim"))
-	#pj.visible=true
-	#pj.can_move = true
+	#tween.connect("finished",Callable(self,"comic_anim"))
+	pj.visible=true
+	pj.can_move = true
 
 func comic_anim():
 	await get_tree().create_timer(0.6).timeout  # Espera a que termine la estrofa
@@ -320,17 +320,17 @@ func room4_camera():
 	#METER ANIM DEL FINAL DEL JUEGO
 func decision_final():
 	ScriptGlobal.musica_final()
-	if cont_decision<0:
+	if ScriptGlobal.cont_decision<0:
 		print("final a")
 		#$Finales.play("FinalA")
-		$EstrofaAnim6/AnimationPlayer.play("estrofa")
+		$EstrofaAnim7/AnimationPlayer.play("estrofa")
 		await get_tree().create_timer(39).timeout  #espera a que termine la estrofa
 		ScriptGlobal.goto_scene("res://Scenes/mainmenu.tscn")
 		
 	else:
 		print("final b")
 		#$Finales.play("FinalB")
-		$EstrofaAnim7/AnimationPlayer.play("estrofa")
+		$EstrofaAnim6/AnimationPlayer.play("estrofa")
 		await get_tree().create_timer(39).timeout  #espera a que termine la estrofa
 		ScriptGlobal.goto_scene("res://Scenes/mainmenu.tscn")
 	#tween.connect("finished",Callable(self,"room4_camera_1"))
